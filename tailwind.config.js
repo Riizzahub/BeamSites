ff04d09a44b765f4a71f9e83d83c2298108767db
Riizzahub <aporhyaunpai@gmail.com>
2025-07-13 08:54:46 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-```
+{
+     "name": "my-vercel-site",
+     "version": "0.1.0",
+     "private": true,
+     "scripts": {
+       "dev": "next dev",
+       "build": "next build",
+       "start": "next start",
+       "lint": "next lint"
+     },
+     "dependencies": {
+       "next": "14.2.3",
+       "react": "^18",
+       "react-dom": "^18"
+     },
+     "devDependencies": {
+       "autoprefixer": "^10.4.19",
+       "postcss": "^8.4.38",
+       "tailwindcss": "^3.4.1",
+       "typescript": "^5.5.2"
+     }
+   }
