e628e0212dba587eb11397e04c57345876d3aab8
Riizzahub <aporhyaunpai@gmail.com>
2025-07-13 07:31:58 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Beam Sites',
+  title: 'Immortal Sites',
   description: 'View all of our Roblox links',
 };
 
